0b9967f7234b05f02bbb7d3bba0388856a1e5d3b
GodFlowDix <mdixnpnf@gmail.com>
2019-06-28 00:01:03 -0400

    Swift11, SwiftUI using 3 views
    I practiced combining three views using strictly swiftUI in the IOS 11 beta 2. No storyboard

--- counter/ContentView.swift
+++ counter/ContentView.swift
@@ -7,14 +7,42 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView : View {
+    
     var body: some View {
-        Text("Hello World")
+         VStack {
+            
+            MapView()
+                .edgesIgnoringSafeArea(.top) //makes it seemless with the phone screen
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)  //overlapping whats above me
+                .padding(.bottom, -130) //brings whats under me closer
+        VStack(alignment: .leading) {
+            
+           
+            Text("Turtle Rock")
+                .font(.title)
+            
+            HStack (alignment: .top){
+                        Text("Joshua Tree National Park")
+                            .font(.subheadline)
+                    Spacer() //Puts space between two text
+                        Text("California")
+                            .font(.subheadline)
+                }
+            }
+            .padding() //gives details breathing room
+            Spacer()
+        }
+
     }
 }
 
-#if DEBUG
+
+#if DEBUG //defectives
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
         ContentView()
